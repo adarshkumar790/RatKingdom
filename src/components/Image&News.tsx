db3f9cefae233c9f857fd348d81baead0044654e
AdarhKumar <adarshnit420@gmail.com>
2025-01-29 12:43:02 +0530

    first commit

--- src/components/Image&News.tsx
+++ src/components/Image&News.tsx
@@ -68,7 +68,7 @@ const News: React.FC = () => {
           <button
             key={tab}
             onClick={() => handleTabClick(tab as "Spot" | "Feature" | "Margin" | "Options")}
-            className={`px-6 py-2 font-semibold rounded-lg transition-all duration-200 ${
+            className={`md:px-6 px-4 py-2 font-semibold rounded-lg transition-all duration-200 ${
               selectedTab === tab
                 ? "text-gray-600 shadow"
                 : "text-black hover:bg-gray-200"
