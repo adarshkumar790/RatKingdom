317d41cc158402507108e67908ecea22347d2e4b
AdarhKumar <adarshnit420@gmail.com>
2025-01-29 13:21:43 +0530

    first commit

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -40,7 +40,7 @@ const FAQ: React.FC = () => {
 
   return (
     <div className="max-w-7xl mx-auto p-4">
-      <h1 className="md:text-3xl text-2xl  font-semibold text-center mb-6 text-[#000000]">Frequently Asked Questions</h1>
+      <h1 className="md:text-3xl text-xl  font-semibold text-center mb-6 text-[#000000]">Frequently Asked Questions</h1>
       <div className="md:space-y-4 space-y-2 w-full">
         {faqItems.map((item, index) => (
           <div
